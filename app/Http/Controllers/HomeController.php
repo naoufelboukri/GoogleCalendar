943afad003539371dbb32e966b9e0615e9521d74
Naoufel <naoufelboukri@gmail.com>
2022-03-18 03:01:29 +0100

    Refactor and add json output method

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,10 +10,30 @@ class HomeController extends Controller
 
     // This function return the principal view of the calendar 
     public function index(){
+        return view('welcome') ; 
+        // ->format('Y-m-d H:i:s') 
+    }
+
+    //This function is used to create new events 
+    public function create(){
+        return view('form') ; 
+    }
+
+    //This function stores the input data register by the customer and push them on the database 
+    public function store(Request $request){
+        Calendar::create([
+            'title' => $request->title , 
+            'start' => $request->start , 
+            'end' => $request->end , 
+            'description' => $request->description , 
+            'all_day' => $request->all_day , 
+        ]) ; 
+    }
+
+    // This function returns the json data 
+    public function source(){
         // Get all my calendar events 
         $events = Calendar::all() ;
-        
-        // ->format('Y-m-d H:i:s') 
 
         $tab_events = array() ; 
 
@@ -28,28 +48,6 @@ class HomeController extends Controller
             ] ; 
         }
 
-        $data = json_encode($tab_events) ; 
-
-        return view('welcome' , compact('data')) ; 
-    }
-
-    //This function is used to create new events 
-    public function create(){
-        return view('form') ; 
-    }
-
-    public function show($id){
-        $event = Calendar::findOrFail($id) ; 
-
-        $data = [
-            'id' => $event->id , 
-            'title' => $event->title,
-            'start' => $event->start,
-            'end' => $event->end,
-            'description' => $event->description,
-            'all_day' => $event->all_day           
-        ] ; 
-
-        return response()->json($data) ; 
+        return response()->json($tab_events) ; 
     }
 }
